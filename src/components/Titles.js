3d204a77c4caea00644f727c19a6da1c568f0ce9
jeetwide <wide.angle456@gmail.com>
2019-07-30 14:46:25 +0530

    second commit

---
+++ src/components/Titles.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Titles = props => {
+  return (
+    <div>
+      <h1 className="title-container__title">Weather Search</h1>
+      <h3 className="title-container__subtitle" >Find out temperature, conditions and more....</h3>
+    </div>
+  );
+};
+/* 
+class Titles extends React.Component {
+    render(){
+        return(
+            <div>
+                <h1>Weather Finder</h1>
+                <p>Find out temperature, conditions and more....</p>
+            </div>
+        );
+    }
+} */
+
+export default Titles;
